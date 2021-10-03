b7f467d05eb748cf0eeb97a740d72e9623ec50ae
flyactiv <flyactiv46@gmail.com>
2021-10-03 21:37:12 +0300

    Add a handler for button

--- Alarm/app/src/main/java/com/example/alarm/MainActivity.java
+++ Alarm/app/src/main/java/com/example/alarm/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.alarm;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button setAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setAlarm = findViewById(R.id.alarm_button);
+
+        setAlarm.setOnClickListener(v -> {
+            MaterialTimePicker materialTimePicker = new MaterialTimePicker.Builder()
+                    .setTimeFormat(TimeFormat.CLOCK_24H)
+                    .setHour(12)
+                    .setMinute(0)
+                    .setTitleText("Выберите время для будильника")
+                    .build();
+
+            materialTimePicker.addOnPositiveButtonClickListener(view -> {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.SECOND, 0);
+                calendar.set(Calendar.MILLISECOND, 0);
+                calendar.set(Calendar.MINUTE, materialTimePicker.getMinute());
+                calendar.set(Calendar.HOUR_OF_DAY, materialTimePicker.getHour());
+
+            });
+
+            materialTimePicker.show(getSupportFragmentManager(), "tag_picker");
+        });
     }
 }
